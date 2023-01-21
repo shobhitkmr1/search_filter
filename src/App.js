56626fb66224fa5fbfa6137e7b56794022cfcf85
Shobhitkmr <43270097+shobhitkmr1@users.noreply.github.com>
2023-01-21 10:22:32 +0530

    Final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
+import JSONFILE from "./Testing_Data.json";
 function App() {
+  const [searchValue, setSearchValue] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(event) => {
+          setSearchValue(event.target.value);
+        }}
+      />
+      {JSONFILE.filter((val) => {
+        if (searchValue == "") {
+          return val;
+        } else if (
+          val.first_name.toLowerCase().includes(searchValue.toLowerCase())
+        ) {
+          return val;
+        }
+      }).map((val, key) => {
+        return (
+          <div className="user" key={key}>
+            <p>{val.first_name}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
